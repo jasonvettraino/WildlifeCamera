338a59806e0a2f971f6a8ac49a09d0a60470d5d8
Jason Vettraino <Jason.Vettraino@gmail.com>
2021-04-22 21:39:45 +0100

    Release 1.0.1 of WildlifePICamera

---
+++ AWS/GreenGrass/artifacts/com.example.WildlifePiCamera/1.0.1/AWSpir01.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+from datetime import datetime
+import subprocess
+import boto3
+ 
+GPIO.setmode(GPIO.BCM)
+pirPin = 7
+GPIO.setup(pirPin, GPIO.IN)
+
+s3_resource = boto3.resource('s3')
+ 
+try:
+	while True:
+		if (GPIO.input(pirPin)):
+			print ("Motion Detected!")
+			now = datetime.now()
+			dtString = now.strftime("%Y-%m-%d-%H-%M-%S") 
+			print (dtString) 
+			filename =dtString+'.jpg'
+			cmd = 'raspistill -o ' + filename
+			pid=subprocess.call(cmd, shell=True)
+
+			first_object = s3_resource.Object(
+##			bucket_name="inputimage.cloudystuff.info", 
+				bucket_name=sys.argv[1], 
+				key=filename)
+
+			first_object.upload_file(filename) 
+		else:
+			print ("No motion")
+        
+		time.sleep(1)
+except KeyboardInterrupt:
+	GPIO.cleanup()
