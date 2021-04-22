592e07629d6929dd91197c1169c8fd2dd0d63ea6
Jason Vettraino <Jason.Vettraino@gmail.com>
2021-04-22 20:50:45 +0100

    Converted to Python3

--- AWS/GreenGrass/artifacts/com.example.WildlifePiCamera/1.0.0/AWSpir01.py
+++ AWS/GreenGrass/artifacts/com.example.WildlifePiCamera/1.0.0/AWSpir01.py
@@ -12,25 +12,25 @@ GPIO.setup(pirPin, GPIO.IN)
 s3_resource = boto3.resource('s3')
  
 try:
-    while True:
-        if (GPIO.input(pirPin)):
-            print "Motion Detected!"
-	    now = datetime.now()
-       	    dtString = now.strftime("%Y-%m-%d-%H-%M-%S") 
-	    print dtString 
-	    filename =dtString+'.jpg'
-	    cmd = 'raspistill -o ' + filename
-	    pid=subprocess.call(cmd, shell=True)
+	while True:
+		if (GPIO.input(pirPin)):
+			print ("Motion Detected!")
+			now = datetime.now()
+			dtString = now.strftime("%Y-%m-%d-%H-%M-%S") 
+			print (dtString) 
+			filename =dtString+'.jpg'
+			cmd = 'raspistill -o ' + filename
+			pid=subprocess.call(cmd, shell=True)
 
-	    first_object = s3_resource.Object(
+			first_object = s3_resource.Object(
 ##			bucket_name="inputimage.cloudystuff.info", 
-            bucket_name=sys.argv[1], 
-            
-			key=filename)
+				bucket_name=sys.argv[1], 
+				key=filename)
 
-	    first_object.upload_file(filename) 
-	else:
-            print "No motion"
-        time.sleep(1)
+			first_object.upload_file(filename) 
+		else:
+			print ("No motion")
+        
+		time.sleep(1)
 except KeyboardInterrupt:
-    GPIO.cleanup()
+	GPIO.cleanup()
